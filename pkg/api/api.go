a95dc5c079ecd71754416c9fae5fec9ebe27ba45
Hon Yi Hao <165232024+yihao03@users.noreply.github.com>
2026-09-24 15:50:03 +0800

    Update pkg/api/api.go
    Co-authored-by: Andrew <159703254+andrewsoonqn@users.noreply.github.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -49,6 +49,7 @@ func httpHandler[E any](env *E, handler Handler[E], timeout time.Duration) http.
 		go func() {
 			defer func() {
 				if value := recover(); value != nil {
+          slog.ErrorContext(r.Context(), "handler panic", "panic", value, "stack", string(debug.Stack()))
 					panics <- value
 				}
 			}()
