b3021e98e8b49464ba018fd8e5b05ee7b92bf94e
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-09-25 12:40:45 +0800

    Mount supplier rpc in router

--- user-service/internal/router/router.go
+++ user-service/internal/router/router.go
@@ -9,8 +9,8 @@ import (
 	appmiddleware "github.com/AY2627S1-CS3219-P1/FoC/user-service/internal/router/middleware"
 	"github.com/AY2627S1-CS3219-P1/FoC/user-service/internal/router/routes"
 	"github.com/AY2627S1-CS3219-P1/FoC/user-service/internal/router/routes/adminroutes"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func Setup(env *deps.Env) *chi.Mux {
